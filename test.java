6c0f0237598ca3d2041ca466f31139a79bf13556
JohnCarloBacsa <bacsajohncarlo18@gmail.com>
2025-09-26 18:25:10 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,12 @@
+public class if_statement
+  {
+    public static void main{String[] args)//execution begins here
+      {
+
 int counter = 5:
   while (counter >= 0) {
   System.out.println(counter);
   counter--;
 }
+    }
+                           }
